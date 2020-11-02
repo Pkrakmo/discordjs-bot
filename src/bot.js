27fd75f9e8b7e7302acba791395d2a7938c26ee0
Paul <paul.krakmo@payex.com>
2020-11-02 14:37:48 +0100

    Smol update

--- src/bot.js
+++ src/bot.js
@@ -13,9 +13,9 @@ client.on('message', (message) =>
     //ignores the bot
     if (message.author.bot) return;
 
-    var correctTime = ["Indeed it is", "On time !", "Hello there", "Bingo"];
-    var lateTime = ["Slowpoke.png", "Please try again later"];
-    var earlyTime = ["Bit to early there, Buckaroo ?", "Fastes shooter in the west, eh ?", "This is not the time you are looking for", "Please try again later", "You're a bit too early "];
+    var correctTime = ["Indeed it is", 'https://media.giphy.com/media/Nx0rz3jtxtEre/giphy.gif', 'https://media.giphy.com/media/236NoPWibFgVi8lBgi/giphy.gif'];
+    var lateTime = ['https://media.giphy.com/media/lgIyvBoSKEhuo/giphy.gif', "Please try again later", "You're a bit too late"];
+    var earlyTime = ["Bit to early there, Buckaroo ?", "Fastes shooter in the west, eh ?", "This is not the time you are looking for", "Please try again later", "You're a bit too early ", 'https://media.giphy.com/media/6uGhT1O4sxpi8/giphy.gif'];
     
     var today = new Date();
     var time = today.getHours() + ":" + today.getMinutes();
@@ -23,16 +23,16 @@ client.on('message', (message) =>
 
     if (message.content === theAnswer){
         if (time === theAnswer){
-            var randomCorrect = Math.floor(Math.random() * correctTime.length)
-            message.channel.send(correctTime[randomCorrect])
+            var random = Math.floor(Math.random() * correctTime.length)
+            message.channel.send(correctTime[random])
         }
         else if (time > theAnswer){
-            var randomLate = Math.floor(Math.random() * lateTime.length)
-            message.channel.send(lateTime[randomLate])
+            var random = Math.floor(Math.random() * lateTime.length)
+            message.channel.send(lateTime[random])
         }
         else if (time < theAnswer){
-            var randomEarly = Math.floor(Math.random() * earlyTime.length)
-            message.channel.send(earlyTime[randomEarly])
+            var random = Math.floor(Math.random() * earlyTime.length)
+            message.channel.send(earlyTime[random])
         }      
     }
 
