3b7c62434caa6e6d9b600176d509591ef1937d06
Paul <paul.krakmo@payex.com>
2020-11-03 20:12:22 +0100

    Added features and fixed some stuff
    Added ayaya, !commands and some new time responses
    Fixed timebug, hopefully

--- src/bot.js
+++ src/bot.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 
 const { Client } = require('discord.js')
 const client = new Client();
+const PREFIX = "!"
 
-client.on('ready', () => {
+client.on('ready',  () => {
     console.log(`${client.user.tag} has logged in`)
 });
 
@@ -15,48 +16,87 @@ client.on('message', (message) =>
 
     var correctTime = 
     [`Indeed it is, ${message.author.username}`, 
-    'https://media.giphy.com/media/Nx0rz3jtxtEre/giphy.gif', 
+    'https://media.giphy.com/media/Nx0rz3jtxtEre/giphy.gif',
+    'https://youtu.be/-YCN-a0NsNk',
+    'https://www.youtube.com/watch?v=69AyYUJUBTg',
+    'https://media.giphy.com/media/8xgqLTTgWqHWU/giphy.gif',
     'https://media.giphy.com/media/236NoPWibFgVi8lBgi/giphy.gif'];
 
     var lateTime = 
-    ['https://media.giphy.com/media/lgIyvBoSKEhuo/giphy.gif', 
-    `You're a bit too late, ${message.author.username}`,
+    [`You're a bit too late, ${message.author.username}`,
     "You're a bit too late",
     "This is not the time you are looking for",
-    'https://media.giphy.com/media/6uGhT1O4sxpi8/giphy.gif',
+    'https://i.imgflip.com/4kvu43.jpg',
     `Try again tomorrow, ${message.author.username}`];
 
-    var earlyTime = [`Bit to early there, ${message.author.username} ?`, 
+    var earlyTime = 
+    [`Bit to early there, ${message.author.username} ?`, 
     `Ladies and gentlemen, the fastest shooter in the west: ${message.author.username}`, 
     "This is not the time you are looking for", 
     "Please try again later", 
-    "You're a bit too early ", 
-    'https://media.giphy.com/media/6uGhT1O4sxpi8/giphy.gif'];
+    "You're a bit too early "];
     
-    var today = new Date();
-    var time = `${today.getHours()}${today.getMinutes()}`;
+   
     var theAnswer = '13:37'
     var helper = theAnswer.replace(':', '');
+    
+    var today = new Date();
+    var time = today.toLocaleTimeString();
+    var timeCorrector = time.slice(0, 5).replace(':', '');
 
     if (message.content === theAnswer || message.content === helper){
-        if (parseFloat(time)  === parseFloat(helper) ){
+
+
+
+        if (timeCorrector  === helper ){
             var random = Math.floor(Math.random() * correctTime.length)
             message.channel.send(correctTime[random])
 
         }
 
-        else if ( parseFloat(time)  > parseFloat(helper) ){
+        else if (timeCorrector > helper ){
             var random = Math.floor(Math.random() * lateTime.length)
             message.channel.send(lateTime[random])
 
         }
 
-        else if (parseFloat(time)  <  parseFloat(helper) ){
+        else if (timeCorrector  <  helper ){
             var random = Math.floor(Math.random() * earlyTime.length)
             message.channel.send(earlyTime[random])
         }      
     };
 
-});
+    if (message.content === 'ayaya'){
+
+        message.channel.send('https://youtu.be/9wnNW4HyDtg')
+
+    };
+
+    const commands = ["commands", "placeholder"]
+
+    if (message.content.startsWith(PREFIX)){
+        if (message.author.bot) return;
+        const [CMD_NAME, ...args] = message.content
+            .trim()
+            .substring(PREFIX.length)
+            .split((/\s+/))
+
+            if (CMD_NAME === 'commands'){
+            message.channel.send("Command list:")
+            for (i = 0; i < commands.length; i++) {
+                message.channel.send(`${PREFIX}${commands[i]}`);
+              }}
+
+            if (CMD_NAME === 'placeholder'){
+                message.channel.send("placeholder")
+            }
+                
+            if (CMD_NAME === 'debug'){
+                console.log(`${time} ${message.author.username} tried to debug something, lol`)
+                message.channel.send(`We are the Borg. You will be assimilated. Resistance is futile.`)
+            }
+            
+        };
+    });
 
 client.login(process.env.TESTDISCORDJS_BOT_TOKEN)
