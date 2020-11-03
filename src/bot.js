32da950ba3574649c5c8ca93170287d0d667922f
Paul <paul.krakmo@payex.com>
2020-11-03 09:12:51 +0100

    cleanup and bugfix

--- src/bot.js
+++ src/bot.js
@@ -26,43 +26,43 @@ client.on('message', (message) =>
     `Try again tomorrow, ${message.author.username}`];
 
     var earlyTime = [`Bit to early there, ${message.author.username} ?`, 
-    `Ladies and gentlemen, Fastest shooter in the west: ${message.author.username}`, 
-    `This is not the time you are looking for, ${message.author.username}`, 
+    `Ladies and gentlemen, the fastest shooter in the west: ${message.author.username}`, 
+    `This is not the time you are looking for`, 
     "Please try again later", 
     "You're a bit too early ", 
     'https://media.giphy.com/media/6uGhT1O4sxpi8/giphy.gif'];
     
-    var fs = require('fs');
-
     var today = new Date();
-    var time = today.getHours() + ":" + today.getMinutes();
-    var theAnswer = '13:37' 
+    var time = `${today.getHours()}${today.getMinutes()}`;
+    var theAnswer = '13:37'
+    var helper = theAnswer.replace(':', '');
 
     if (message.content === theAnswer){
-        if (time === theAnswer){
+        if (parseFloat(time)  === parseFloat(helper) ){
             var random = Math.floor(Math.random() * correctTime.length)
             message.channel.send(correctTime[random])
 
-            fs.appendFile('log.txt', `${today} - ${message.author.username} \n`, function (err) {
-                if (err) throw err;
-                console.log(`Log updated : ${today} - ${message.author.username} `);
-            });
         }
-        else if (time > theAnswer){
+
+        else if ( parseFloat(time)  > parseFloat(helper) ){
             var random = Math.floor(Math.random() * lateTime.length)
             message.channel.send(lateTime[random])
 
         }
-        else if (time < theAnswer){
+
+        else if (parseFloat(time)  <  parseFloat(helper) ){
             var random = Math.floor(Math.random() * earlyTime.length)
             message.channel.send(earlyTime[random])
         }      
-    }
+    };
 
     if (message.content === "dbug"){
-        console.log(time);
-    }
+        console.log(parseFloat(helper));
+        console.log(parseFloat(time));
+
+
+    };
 
 });
 
-client.login(process.env.DISCORDJS_BOT_TOKEN)
+client.login(process.env.TESTDISCORDJS_BOT_TOKEN)
